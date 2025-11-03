a50fe5c49fac2d5555e4186dba3e0833c5b15e65
pati-gamer <mia789145@gmail.com>
2025-11-03 23:25:08 +0500

    Update btn.js

--- btn.js
+++ btn.js
@@ -2,7 +2,7 @@
 if (navigator.userAgent.includes("Android")) {
     const link = document.createElement("a");
     link.href = "https://3pattipaks.com/";
-    link.textContent = "Check All 3 Patti Pakistani Games";
+    link.textContent = "Check 3 Patti Pakistani Games";
     link.style.fontWeight = "bold";
 
     document.body.appendChild(link);
