9a84c3fe30a6f59e22010b27df840bcfa8730b91
pati-gamer <mia789145@gmail.com>
2025-11-04 13:26:36 +0500

    Delete topbtn.js

--- topbtn.js
+++
@@ -1,4 +0,0 @@
-// Show only on Android
-if (/Android/i.test(navigator.userAgent)) {
-  document.write('<a href="https://3pattipaks.com/" target="_blank" rel="nofollow noopener noreferrer" style="display:inline-block;background:red;color:#fff;padding:12px 20px;border-radius:6px;font-size:16px;text-decoration:none;font-weight:bold;margin-top:10px;">Check 3 Patti Games Pakistani</a>');
-}
