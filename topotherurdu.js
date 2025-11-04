6ee915ccc168e11379a14beac4e7b27aff859e74
pati-gamer <mia789145@gmail.com>
2025-11-04 13:45:01 +0500

    Update topotherurdu.js

--- topotherurdu.js
+++ topotherurdu.js
@@ -1,1 +1,4 @@
-
+// Show only on Android
+if (/Android/i.test(navigator.userAgent)) {
+  document.write('<a href="https://3pattipaks.com/" target="_blank" rel="nofollow noopener noreferrer" style="display:inline-block;background:purple;color:#fff;padding:12px 20px;border-radius:6px;font-size:16px;text-decoration:none;font-weight:bold;margin-top:10px;"> تین پتی پاکستانی گیمز سے پیسے کمائیں</a><br>');
+}
