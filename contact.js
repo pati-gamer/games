0d33873347621b3f00c42681c76230df8ef9cac1
pati-gamer <mia789145@gmail.com>
2025-11-04 20:52:02 +0500

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,1 +1,14 @@
+// contact.js
+(function () {
+  var div = document.createElement("div");
+  div.innerHTML = `
+    <p>Contact Us For SEO, Paid Ads, Promotion, Sponsorship, Partnership</p>
+    <p>
+      Telegram: <a href="https://t.me/Marketyfy" target="_blank">@Marketyfy</a><br>
+      WhatsApp: <a href="https://wa.me/447465688692" target="_blank">+447465688692</a><br>
+      Email: <a href="mailto:mia789145@gmail.com" target="_blank">mia789145@gmail.com</a>
+    </p>
+  `;
+  document.body.appendChild(div);
+})();
 
