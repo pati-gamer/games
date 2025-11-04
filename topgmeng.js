e8cf8840125051384b22fce5ca695181e116102e
pati-gamer <mia789145@gmail.com>
2025-11-04 13:33:52 +0500

    Update topgmeng.js

--- topgmeng.js
+++ topgmeng.js
@@ -1,1 +1,5 @@
 
+// Show only on Android
+if (/Android/i.test(navigator.userAgent)) {
+  document.write('<a href="https://3pattipaks.com/" target="_blank" rel="nofollow noopener noreferrer" style="display:inline-block;background:green;color:#fff;padding:12px 20px;border-radius:6px;font-size:16px;text-decoration:none;font-weight:bold;margin-top:10px;"> Check All 3Patti Games Pakistan </a><br>');
+}
