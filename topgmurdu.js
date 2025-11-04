5e1792bed71faad31c9e44fc8b42bc2744adb4e8
pati-gamer <mia789145@gmail.com>
2025-11-04 13:18:57 +0500

    Update topgmurdu.js

--- topgmurdu.js
+++ topgmurdu.js
@@ -1,4 +1,4 @@
 // Show only on Android
 if (/Android/i.test(navigator.userAgent)) {
-  document.write('<a href="https://3pattipaks.com/" target="_blank" rel="nofollow noopener noreferrer" style="display:inline-block;background:red;color:#fff;padding:12px 20px;border-radius:6px;font-size:16px;text-decoration:none;font-weight:bold;margin-top:10px;">سب تین پتی پاکستانی گیمز دیکھیں</a>');
+  document.write('<a href="https://3pattipaks.com/" target="_blank" rel="nofollow noopener noreferrer" style="display:inline-block;background:red;color:#fff;padding:12px 20px;border-radius:6px;font-size:16px;text-decoration:none;font-weight:bold;margin-top:10px;">سب تین پتی پاکستانی گیمز دیکھیں</a><br>');
 }
