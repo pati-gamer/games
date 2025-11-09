f724b50025b1b6e12cf5b57420f1139c90e3c68a
pati-gamer <mia789145@gmail.com>
2025-11-09 18:44:36 +0500

    Update contact2.js

--- contact2.js
+++ contact2.js
@@ -2,7 +2,6 @@ document.write(`
   <div style="margin-top:10px; font-size:15px; line-height:1.4;">
     <b>Contact Us For SEO, Paid Ads, Promotion, Sponsorship, Partnership</b><br>
     Telegram: @Marketyfy<br>
-    WhatsApp: +447465688692<br>
     Email: mia789145@gmail.com<br>
   </div>
 `);
